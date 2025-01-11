b252ac2c07726865e6bdcf9255137e88bde21a99
Win <winielsoncm@gmail.com>
2025-01-10 21:28:27 -0600

    Add comments to Anagram solution

--- Anagrams.java
+++ Anagrams.java
@@ -8,7 +8,7 @@ public class Anagrams {
         HashMap<String, Integer> anagramMap = new HashMap();
         List<List<String>> groupedAnagrams = new ArrayList<>();
 
-        for(int i = 0; i < strs.length; i++) {
+        for (int i = 0; i < strs.length; i++) {
             // Step 1: Convert string to character array
             char[] charArray = strs[i].toCharArray();
             // Step 2: Sort the character array
@@ -33,15 +33,21 @@ public class Anagrams {
     public List<List<String>> groupAnagramsBetter(String[] strs) {
         HashMap<String, List<String>> anagramMap = new HashMap();
 
-        for(String str: strs) {
+        // Count how many times each letter appears in a string
+        for (String str : strs) {
+            // 1. create an integer array of 26 characters, since there are 26 letters
             int[] letterCount = new int[26];
 
-            for(char c: str.toCharArray()) {
+            // 2. Go through each character in each string and increment the position in the int array given the letter
+            for (char c : str.toCharArray()) {
                 letterCount[c - 'a']++; // a - a => 97 - 97 = 0; e - a => 101 - 97 = 4
             }
+            // 3. Convert int array to be a string so that it can act as the key for the anagramMap
             String key = Arrays.toString(letterCount);
-            System.out.println("Key:"+key);
+            System.out.println("Key:" + key);
+            // 4. Add key to map if not already there
             anagramMap.putIfAbsent(key, new ArrayList<>());
+            // 5. Add string to the key's value (which is an array of strings)
             anagramMap.get(key).add(str);
         }
 
@@ -50,7 +56,7 @@ public class Anagrams {
 
     public static void main(String[] args) {
         Anagrams instance = new Anagrams();
-        String[] input = {"act","pots","tops","cat","stop","hat"};
+        String[] input = {"act", "pots", "tops", "cat", "stop", "hat"};
         List<List<String>> result = instance.groupAnagramsBetter(input);
         System.out.println("Answer: " + result.toString());
     }
