1f6e4a8dc3794f70cb1f86661dd4e4b9a3c35c2f
Win <winielsoncm@gmail.com>
2025-02-13 12:11:33 -0600

    Add solution for PermutationInString

---
+++ PermutationInString.java
@@ -0,0 +1,47 @@
+//https://neetcode.io/problems/longest-substring-without-duplicates
+//Given a string s, find the length of the longest substring without duplicate characters.
+//
+//A substring is a contiguous sequence of characters within a string.
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class PermutationInString {
+    public static String sortString(String string) {
+        if (string == null || string.isEmpty()) return string;
+        char[] chars = string.toCharArray();
+        Arrays.sort(chars); // Sorts in natural order (lexicographical)
+        return new String(chars);
+    }
+
+    public boolean checkInclusion(String s1, String s2) {
+        String sorted1 = sortString(s1);
+        int l = 0, r = s1.length();
+
+        while (r <= s2.length()) {
+            System.out.println("sorted1: " + sorted1 + "; sortString(s2.substring(l, r)): " + sortString(s2.substring(l, r)) + "; l: " + l + "; r: " + r);
+            // NOTE: for s2.substring(l, r), the endIndex r is +1 of the substring
+            // ex: for 'abc', 'abc'.substring(0, 2) == 'ab'
+            if (Objects.equals(sorted1, sortString(s2.substring(l, r)))) {
+                return true;
+            }
+
+            l++;
+            r++;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PermutationInString instance = new PermutationInString();
+        String input1 = "adc";
+        String input2 = "dcda";
+        boolean result = instance.checkInclusion(input1, input2);
+        System.out.println("Answer: " + result);
+    }
+}
+
+
+
+
